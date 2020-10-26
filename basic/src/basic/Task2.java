2d31bc99ba2c23cdec8e25c073d966e816cc1d0f
Natsuko1021 <natsu8chary5@keio.jp>
2020-10-26 22:38:00 +0900

    課題提出 - Javaの基礎

---
+++ basic/src/basic/Task2.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+/*hogehoge2の位置が｝の外に出ていたため、内に配置した
+ */
+	System.out.println("hogehoge");
+	System.out.print("hogehoge2");
+	}
+
+}
